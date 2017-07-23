f64035c1ebaa9b5f60cfff84102f38b7481c46cd
Javier Alvarado <alvarado.ito@gmail.com>
2017-07-23 13:07:31 -0500

    Data Access

---
+++ ProyectoCine_Grupo3/CoreCine/DAAsiento.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCine
+{
+    public class DAAsiento
+    {
+
+        static public List<Asiento> listado()
+        {
+            List<Asiento> listado = new List<Asiento>();
+
+            using (var data = new CineDBEntities())
+            {
+                listado = data.Asiento.ToList();
+            }
+
+            return listado;
+        }
+
+        static bool RegistrarAsiento(Asiento asiento)
+        {
+            bool exito = true;
+            using (var data = new CineDBEntities)
+            {
+                data.Asiento.Add(asiento);
+                data.SaveChanges();
+            }
+
+            return exito;
+        }
+
+        static bool EliminarAlumno(string codAsiento)
+        {
+            bool exito = true;
+
+            using (var data = new CineDBEntities())
+            {
+                Asiento actual = data.Asiento.Where(x => x.CodAsiento == codAsiento).FirstOrDefault();
+                data.Asiento.Remove(actual);
+                data.SaveChanges();
+            }
+
+            return exito;
+        }
+
+
+    }
+}
